f006831f788f6757e47d8d4284deef3f5debf9da
lea-lp <lea.leipp@gmail.com>
2018-12-04 03:55:28 +0100

    mep google map index

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -109,4 +109,22 @@ var Wavify = (function() {
 
 })();
 
+	// Google Maps 
+	var onMapMouseleaveHandler = function(event) {
+		$('#map-notice').fadeIn(500);
+		var elemento = $(this);
+		elemento.on('click', onMapClickHandler);
+		elemento.off('mouseleave', onMapMouseleaveHandler);
+		$('.map-overlay').fadeIn(500);
+	}
+	var onMapClickHandler = function(event) {
+		$('#map-notice').fadeOut(500);
+		var elemento = $(this);
+		elemento.off('click', onMapClickHandler);
+		$('.map-overlay').fadeOut(500);
+		elemento.on('mouseleave', onMapMouseleaveHandler);
+	}
+	$('.map.embed-container').on('click', onMapClickHandler);
+	
+
 });
