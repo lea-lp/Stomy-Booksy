937b795c1c701f444b0b08c8445e312ec21c8d30
Jango <arthur.vouloir@gmail.com>
2018-12-05 18:50:31 +0100

    Profile page Establishment done

--- app/controllers/establishments_controller.rb
+++ app/controllers/establishments_controller.rb
@@ -6,13 +6,10 @@ class EstablishmentsController < ApplicationController
     @sub_cat_hash = Hash.new
     @establishment.teachers.each do |teacher|
       teacher.sub_categories.each do |sub_cat|
-        @sub_cat_hash[sub_cat.category.title+" - "+sub_cat.title] ||= []
-        @sub_cat_hash[sub_cat.category.title+" - "+sub_cat.title] << teacher
+        @sub_cat_hash[sub_cat.title] ||= []
+        @sub_cat_hash[sub_cat.title] << teacher
       end
     end
-    puts "*" *60
-    puts @sub_cat_hash
-    puts "*" *60
   end
 
 
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,6 @@
 class StudentsController < ApplicationController
   def show
+  	@student = Student.find(params[:id])
   end
 
   def edit
