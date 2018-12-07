edaaec176d5417a013d4a59ddbb3584efc1931bd
Jango <arthur.vouloir@gmail.com>
2018-12-07 18:42:16 +0100

    Page edit done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_devise_parameters, if: :devise_controller?
 
   def configure_devise_parameters
-    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:last_name, :first_name, :email, :phone, :siret, :description, :password, :password_confirmation)}
+    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:name,:address, :last_name, :first_name, :email, :phone, :siret, :description, :password, :password_confirmation)}
+    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:name, :last_name, :first_name, :email, :phone, :siret, :description, :password, :password_confirmation, :current_password)}
   end
 
   def filter_on_signed_in
