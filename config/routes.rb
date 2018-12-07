7e3f9da6dcdd4e97342887b67abe95e92a9e0d89
Thierry <kasskq@gmail.com>
2018-12-07 10:25:23 +0100

    cleaning routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,18 @@ Rails.application.routes.draw do
   devise_for :teachers
   devise_for :students
   root 'home#index'
-  resources :students, only: [:show, :edit]
-  resources :teachers, only: [:show, :edit] do
+  resources :students, only: [:show]
+  resources :teachers, only: [:show] do
     resources :teacher_establishments, only: [:destroy]
     resources :teacher_cats, only: [:destroy]
   end
   resources :establishments, only: [:show, :edit]
-  delete '/establishments/:establishment_id/teachers/:teacher_id', to: 'establishments#destroy_relation_teach_esta', as: :destroy_relation_teach_esta
-  delete '/teachers/:teacher_id/sub_categories/:sub_category_id', to: 'teachers#destroy_relation_teacher_subcat', as: :destroy_relation_teacher_subcat
   resources :resources, only: [:create, :destroy, :edit]
   get '/dashboard', to: 'home#dashboard', as: 'dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+  
+
+# just in case
+  # delete '/establishments/:establishment_id/teachers/:teacher_id', to: 'establishments#destroy_relation_teach_esta', as: :destroy_relation_teach_esta
+  # delete '/teachers/:teacher_id/sub_categories/:sub_category_id', to: 'teachers#destroy_relation_teacher_subcat', as: :destroy_relation_teacher_subcat
