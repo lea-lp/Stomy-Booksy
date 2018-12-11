4cbda6fd467fc875d38ad83d1d627d9308fa216b
Clem Baldy <clemoun@yahoo.fr>
2018-12-11 11:49:01 +0100

    choice of hour/minute separated from date + Gmaps API key changed

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,6 +8,11 @@ class EventsController < ApplicationController
 
     #to build the "new event" form
     @duration_picker = [["1h", 3600], ["30min", 1800]]
+    @hours = []
+    (8..20).to_a.each do |i| 
+      @hours << [i.to_s,i.to_s]
+    end
+    @minutes = [["00", "00"], ["15", "15"], ["30", "30"], ["45", "45"]]
 
     #all the objects used in the view
     @teacher = Teacher.find(params[:teacher_id])
@@ -37,6 +42,11 @@ class EventsController < ApplicationController
   end
 
   def create
+    params[:event][:start_time] = params[:event][:date]+"T"+params[:event][:hours]+":"+params[:event][:minutes]
+    puts "$"*60
+    puts params[:event][:start_time]
+    puts "$"*60
+
     @event = Event.new(event_params)
     @event.set_event_name
 
