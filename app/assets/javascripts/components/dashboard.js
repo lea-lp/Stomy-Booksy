b2419d44b36a62974245c3f8bdb5657726822bd4
lea-lp <lea.leipp@gmail.com>
2018-12-11 23:42:43 +0100

    add js dashboard estab

---
+++ app/assets/javascripts/components/dashboard.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+
+    // HIDE
+    $("#hide-rdv").hide();
+    $("#hide-resource-list").hide();
+    $("#hide-resource-add").hide();
+    $("#hide-calendar").hide();
+
+    // SHOW
+    $("#rdv").click(function(){
+        $("#hide-rdv").toggle();
+    });
+    $("#list-resource").click(function(){
+        $("#hide-resource-list").toggle();
+    });
+    $("#add-resource").click(function(){
+        $("#hide-resource-add").toggle();
+    });
+    $("#calendar").click(function(){
+        $("#hide-calendar").toggle();
+    });
+
+});
