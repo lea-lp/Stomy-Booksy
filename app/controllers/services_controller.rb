837458facf19a8a5301d2ec7b90448223950e83c
Clem Baldy <clemoun@yahoo.fr>
2018-12-12 14:24:26 +0100

    profils ok with services

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,14 @@
 class ServicesController < ApplicationController
   def index
+    if params[:establishment_id]
+      @establishment = Establishment.find(params[:establishment_id])
+      @services = @establishment.services
+    elsif params[:teacher_id]
+      @teacher = Teacher.find(params[:teacher_id])
+      @services = @teacher.services
+    else
+      @services = Service.all
+    end
   end
 
   def show
