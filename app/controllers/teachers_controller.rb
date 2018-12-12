6236014b70e037a189e4cb625f258263324b10f1
Clem Baldy <clemoun@yahoo.fr>
2018-12-12 17:53:54 +0100

    Estab Dashboard in progress

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -2,7 +2,7 @@ class TeachersController < ApplicationController
 
   def show
     @teacher = Teacher.find(params[:id])
-    @sub_categories = SubCategory.all
+    @sub_categories = SubCategory.all.order(title: :asc)
     @sub_categories =  @sub_categories.select {|s| !@teacher.sub_categories.include?(s)}
   end
 
