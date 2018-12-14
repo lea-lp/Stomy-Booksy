2ec5ceea326bc15e6bc79673a50bd06adde4db37
Clem Baldy <clemoun@yahoo.fr>
2018-12-14 13:48:54 +0100

    changing index sentence

--- app/helpers/events_calendar_helper.rb
+++ app/helpers/events_calendar_helper.rb
@@ -10,29 +10,4 @@ module EventsCalendarHelper
   def event_height(event)
     "height: #{event.duration * 40/3600}px;"
   end
-  
-  # def at_location(appointment)
-  #   " at #{appointment.location_name}" if appointment.location
-  # end
-  
-  #Mets en valeur l'event (rajout de la classe highlight) si on est sur la page de show ou edit. A voir si on utilise...
-  # def highlight_event(event)
-  #   if current_page?( event_path(event)) || current_page?( edit_event_path(event) )
-  #     " highlight"
-  #   end
-  # end
-  
-  # def from_to(appointment)
-  #   "#{appointment.start_time.strftime("%l:%M %p")} - #{appointment.end_time.strftime("%l:%M %p")}"
-  # end
-  
-  # def date_of_next(day, start_date)
-  #   date = Date.parse(day)
-  #   delta = date >= start_date ? 0 : 7
-  #   date + delta
-  # end
-  
-  # def date_of_last(day)
-  #   date_of_next(day) - 7.days
-  # end
 end
