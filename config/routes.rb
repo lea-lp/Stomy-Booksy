366bc7df9afe5bbf699567c9f1b3fc774aa5d448
Clem Baldy <clemoun@yahoo.fr>
2018-12-14 15:24:03 +0100

    bug fixed: dashboard modify profile btn + suppressed link on profile picture + in profile show telephone left and email right

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,18 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  resources :students, only: [:show, :edit] do
+  resources :students, only: [:show] do
     get '/dashboard', to: 'students#dashboard', as: 'dashboard'
   end
 
-  resources :teachers, only: [:index, :show, :edit] do
+  resources :teachers, only: [:index, :show] do
     resources :establishments, only: [:index]
     resources :services, only: [:index]
     resources :teacher_cats, only: [:create, :destroy]
     get '/dashboard', to: 'teachers#dashboard', as: 'dashboard'
   end
   
-  resources :establishments, only: [:index, :show, :edit] do
+  resources :establishments, only: [:index, :show] do
     resources :teachers, only: [:index]
     resources :services, only: [:index]
     get '/dashboard', to: 'establishments#dashboard', as: 'dashboard'
